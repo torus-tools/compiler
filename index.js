5efb4fc5cc0697bbb581ea8fa49b367e95977ce4
gkpty <gkardonski@gmail.com>
2020-03-11 21:47:13 -0500

    added components and component function also modified some styling and added more info for projects

--- index.js
+++ index.js
@@ -2,19 +2,40 @@ var fs = require('fs')
 
 function replaceObjects(){
   var objArray = [];
-  //read all objects
+  var compArray = [];
+  
+  //create an array of objects
   fs.readdirSync('objects').forEach(function(filename){
     if(filename.includes(".json")){
       let name = filename.split('.json')[0];
       objArray.push(name);
     }
   });
+  //create an array of components
+  fs.readdirSync('components').forEach(function(filename){
+    if(filename.includes(".html")){
+      let name = filename.split('.html')[0];
+      compArray.push(name);
+    }
+  });
+
   //read all templates
   fs.readdirSync('templates').forEach(function(filename){
     if(filename.includes(".html")){
       fs.readFile(`templates/${filename}`, 'utf8', function(err, originFile){
         if(err) throw new Error(err)
         else{
+
+          //replace components
+          for(let comp of compArray){
+            let htmlComp = fs.readFileSync(`components/${comp}.html`, 'utf8')
+            let tag = `<#${comp.toUpperCase()}>`
+            if(originFile.includes(tag)){
+              originFile = originFile.replace(tag, htmlComp);
+            }
+          }
+          
+          //replace objects
           for(let obj of objArray){
             let tag = obj.toUpperCase();
             if(originFile.includes(`<${tag}>`)){
@@ -29,10 +50,11 @@ function replaceObjects(){
                 Object.keys(unit).map(function(key, index){
                   //if the text is between text insert a span tag
                   //insert class toruf-OBJ-VARIABLE in the span tag or the actual tag (if no other text)
+                  //add rules to truncate a particular variable if its length is greater than x
                   temp = temp.replace(`{${key}}`, unit[key]);
                 })
                 htmlObj+=temp
-              });z
+              });
               //insert comment <!-- toruf-OBJ-start -->
               //insert comment <!-- toruf-OBJ-end -->
               let oldObj = `<${tag}>`+objTemplate+`</${tag}>`;
@@ -42,6 +64,7 @@ function replaceObjects(){
               return 'success';
             }
           }
+
         }
       })
     }
