e579ebe89493606f0c510122c557890e61dd2366
gkpty <gkardonski@gmail.com>
2020-03-13 04:20:23 -0500

    finished the reverse compile

--- reverseCompile.js
+++ reverseCompile.js
@@ -27,11 +27,12 @@ function replaceObjects(){
           if(originFile.includes('<!-- <')){
             //for each model
             for(let inst of objArray){
+              
               let start_tag = `<!-- <${inst.toUpperCase()}> -->`;
               let end_tag = `<!-- </${inst.toUpperCase()}> -->`;
               if(originFile.includes(start_tag)){
                 let object = originFile.split(start_tag)[1].split(end_tag)[0];
-                let piece = start_tag + object + end_tag;
+                let rePiece = start_tag + object + end_tag;
                 let rawdata = fs.readFileSync(`objects/${inst}.json`);
                 let db = JSON.parse(rawdata)
                 if(object.includes('id="torufobj_')){
@@ -61,30 +62,60 @@ function replaceObjects(){
                   }
                 }
                 //writeFile
-                //fs.writeFileSync(`objects/${inst}.json`, db)
+                let dbString =  JSON.stringify(db)
+                dbString = dbString.replace(/,/g, ',\n').replace(/{/g, '{\n').replace(/}/g, '\n}')
+                fs.writeFileSync(`objects/reverse${inst}.json`,dbString, 'utf8')
                 //replace Object with object template
+                console.log(inst)
                 let frag1 = object.split('id="torufobj_')[0].split('<')[1]
                 let beginFrag = '<' +frag1+'id="torufobj_'
                 let endPosition = object.split('id="torufobj_')[1].lastIndexOf('<')
-                console.log(endPosition)
                 let endFrag = object.split('id="torufobj_')[1].substr(0, endPosition)    
-                console.log(beginFrag + endFrag)
+                let newPiece = beginFrag + endFrag
+                if(object.includes('id="torufobj_')){
+                  let objId = newPiece.split('id="torufobj_')[1].split('"')[0];
+                  newPiece = newPiece.replace(`id="torufobj_${objId}"`, "")
+                } 
+                if(newPiece.includes('torufattr_')){
+                  let attrArr = newPiece.split('torufattr_');
+                  for(let j=1; j<attrArr.length; j++){
+                    let attrId = attrArr[j].split('"')[0];
+                    let attrVal = attrArr[j].split('>')[1].split('<')[0];
+                    newPiece = newPiece.replace(`<span class="torufattr_${attrId}">${attrVal}</span>`, `{${attrId}}`)
+                  }
+                }
+                if(newPiece.includes('torufhref_')){
+                  let attrArr = newPiece.split('torufhref_');
+                  for(let j=1; j<attrArr.length; j++){
+                    let attrId = attrArr[j].split(' ')[0];
+                    let attrVal = attrArr[j].split('href="')[1].split('"')[0];
+                    newPiece = newPiece.replace(attrVal, `{${attrId}}`);
+                  }
+                }
+                let objstart_tag = `<${inst.toUpperCase()}>
+                `;
+                let objend_tag = `
+                </${inst.toUpperCase()}>`;
+                let objTemplate = objstart_tag + newPiece + objend_tag;
+                originFile = originFile.replace(rePiece, objTemplate)
               }
             }
           } 
 
           //replace components
           for(let comp of compArray){
-            let htmlComp = fs.readFileSync(`components/${comp}.html`, 'utf8')
             let tag = `<#${comp.toUpperCase()}>`
             if(originFile.includes(`<!-- ${tag} -->`)){
-              //console.log(tag)
-              let objTemplate = originFile.split(`<!-- ${tag} -->`)[1].split(`<!-- !${tag} -->`)[0];
-              let piece = `<!-- ${tag} -->` + objTemplate + `<!-- !${tag} -->`;
+              let component = originFile.split(`<!-- ${tag} -->`)[1].split(`<!-- !${tag} -->`)[0];
+              //save component
+              fs.writeFileSync(`components/reverse${comp}.html`, component)
+              let piece = `<!-- ${tag} -->` + component + `<!-- !${tag} -->`;
               originFile = originFile.replace(piece, tag);
             }
           }
-
+          //console.log(originFile)
+          //save file
+          fs.writeFileSync(`templates/reverse${filename}.html`, originFile, 'utf8')
         }
       })
     }
