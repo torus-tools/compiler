5928a6fafd8b15dbaaeecf23cf1e5e1796c1340e
gkpty <gkardonski@gmail.com>
2020-03-14 05:17:52 -0500

    leaner structure with site and toruf created for each theme

---
+++ init.js
@@ -0,0 +1,10 @@
+//create toruf dir
+//create the components dir
+//create the objects dir
+//create the templates dir
+//if there is a site directory
+  //build asset dirs
+  //if the site directory contains html files
+    //reversebuildAll
+//else build the site directory
+  //build asset dirs
